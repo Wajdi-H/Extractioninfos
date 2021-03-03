b60f2ae3113957539c0e3f8faa768ba6afc0d366
Wajdi Hamdi <wajdi.hamdi5@gmail.com>
2021-03-03 16:12:57 +0100

    wajdi03commit

--- src/main/java/yc/espaceclient/extractinfos/ExtractinfosApplication.java
+++ src/main/java/yc/espaceclient/extractinfos/ExtractinfosApplication.java
@@ -6,10 +6,15 @@ import net.sourceforge.tess4j.TesseractException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,8 +24,9 @@ public class ExtractinfosApplication {
     public static void main(String[] args) {
         PieceComptable Pc =new PieceComptable();
           //  File imageFile = new File("/home/wajdi/Downloads/555.bmp");
-                //File imageFile = new File("/home/wajdi/Desktop/PFE/pngimgeconverted/abc.jpg");
-          File imageFile = new File("/home/wajdi/Desktop/PFE/lasttest.jpg");
+            //    File imageFile = new File("/home/wajdi/Desktop/PFE/pngimgeconverted/abc.jpg");
+         File imageFile = new File("/home/wajdi/Desktop/PFE/123456.jpg");
+
 
         ITesseract instance = new Tesseract();  // JNA Interface Mapping
             // ITesseract instance = new Tesseract1(); // JNA Direct Mapping
@@ -32,22 +38,11 @@ public class ExtractinfosApplication {
                     result= result.toUpperCase();
                     result= result.replaceAll(",",".");
                     String [] Tabresult = result.split("\n");
-                   //  Pc.setMontanttotal(Float.parseFloat(Tabresult[1].substring(1,4)));
-
                   for (int i=0 ; i<Tabresult.length;i++){
 
                       if((Tabresult[i].contains("TOTAL"))&&(Tabresult[i].contains("HT"))) {
 
                           try {
-                              /*Pc.setMontanttotalHT(Float.parseFloat(Tabresult[i].chars()            // or codePoints()
-                                      .filter(ch -> Character.isDigit(ch))
-                                      .collect(StringBuilder::new, StringBuilder::appendCodePoint,
-                                              StringBuilder::append)
-                                      .toString()));
-
-                              String Str =Tabresult[i].replaceAll(",",".");
-
-                              Pc.setMontanttotalHT(Float.parseFloat(Str.replaceAll("[a-zA-Z$€]","")));*/
 
                               Pattern p = Pattern.compile("\\d*\\.\\d+");
                               Matcher m = p.matcher(Tabresult[i]);
@@ -68,15 +63,6 @@ public class ExtractinfosApplication {
                       }
                        if (Tabresult[i].contains("TVA")){
                            try {
-                      /*  Pc.setMontantTva(Float.parseFloat(Tabresult[i].chars()
-                                .filter(ch -> Character.isDigit(ch))
-                                .collect(StringBuilder::new, StringBuilder::appendCodePoint,
-                                        StringBuilder::append)
-                                .toString()));
-                               Tabresult[i].replaceAll(",",".");
-
-                               Pc.setMontantTva(Float.parseFloat(Tabresult[i].replaceAll("[a-zA-Z$€]","")));*/
-
 
                                    Pattern p = Pattern.compile("\\d*\\.\\d+");
 
@@ -124,46 +110,61 @@ public class ExtractinfosApplication {
                       }
 
 
-                      if((Tabresult[i].contains("DAT")) ){
+                     // if((Tabresult[i].contains("DAT")) ){
+                          if((Tabresult[i].contains("/")) ){
                           try{
-
-                              Pattern p = Pattern.compile("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+                              Pattern p = Pattern.compile("^(?:(?:31(/|-|\\.)(?:0?[13578]|1[02]))\\1x|(?:(?:29|30)(/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+                             // Pattern p = Pattern.compile("\\d*/*\\d*/\\d*");
                               Matcher m = p.matcher(Tabresult[i]);
                               m.matches();
-                              while(m.find()){
+                              while(m.find()) {
                                   try {
+                                      System.out.println(">> "+ m.group());
+
                                       Pc.setDatepiececomptable(LocalDate.parse(m.group()));
-                                  }
-                                  catch(Exception e){
+
+                                  } catch (Exception e) {
                                       System.out.println("Invalide Date Format");
                                   }
+                              }
 
-                                  System.out.println(">> "+ m.group());
-                              }
-                              if (Pc.getDatepiececomptable()==null){
-                                  Pc.setDatepiececomptable(LocalDate.now());
-                              }
-                          } catch (Exception e ){
+
+                          }
+                              catch (Exception e ){
                               System.out.println("errTOTAL");
                           }
                       }
 
+                      if (Pc.getDatepiececomptable()==null) { // if the date is in format 03 mars 2020
+                          DateFormatSymbols dfs = new DateFormatSymbols(Locale.FRENCH);
+                          String[] FrenshMonths = dfs.getMonths();
+                          for (int l=0 ;l<FrenshMonths.length-1;l++) {
+                              if ((Tabresult[i].contains(FrenshMonths[l].toUpperCase()))) {
+                                  String Matecheddate="";
 
-                      if(((Tabresult[i].contains("FACTURE"))||((Tabresult[i].contains("TICKET")))&&((Tabresult[i].contains("No"))||(Tabresult[i].contains("N°")))  )){
+                                  Pattern p1 = Pattern.compile("[A-Z]*\\d*[A-Z]*\\d+");
+                                  Matcher m1 = p1.matcher(Tabresult[i]);
+                                  m1.matches();
+                                  while (m1.find()){
+                                      try {
+                                            Matecheddate=Matecheddate+m1.group();
+                                           } catch (Exception e) {
+                                          System.out.println("Invalide Date Format");
+                                      }
+                                  }
+                                  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/M/yyyy");
+                                  Pc.setDatepiececomptable(LocalDate.parse(Matecheddate.substring(0,2)+"/"+getmonthnumber(FrenshMonths[l].toUpperCase())+"/"+Matecheddate.substring(2,6), formatter));
+                              }
+
+                          }
+                      }
+                      if(((Tabresult[i].contains("FACTURE"))||((Tabresult[i].contains("TICKET")))&&((Tabresult[i].contains("No"))||(Tabresult[i].contains("N°")) ||(Tabresult[i].contains(":"))))){
                           try{
 
-                         /*     Pattern p = Pattern.compile("^°");
-                              Matcher m = p.matcher(Tabresult[i]);
-                              m.matches();
-
-                              while(m.find()){
-                                  System.out.println(">> "+ m.group());
-
-                              }*/
                                       String[] FactureNumero =Tabresult[i].split(" ");
                                       for(int j=0;j<FactureNumero.length;j++)
                               {
-                                       if((FactureNumero[j].contains("°"))||(FactureNumero[j].contains("NO"))){
+                                       if((FactureNumero[j].contains("°"))||(FactureNumero[j].contains("NO"))||(FactureNumero[j].contains(":"))){
                                            if(j==FactureNumero.length-1){
                                                Pc.setNumfacture(FactureNumero[j].substring(FactureNumero[j].indexOf("°")+1,FactureNumero[j].length()));
                                            }
@@ -176,14 +177,15 @@ public class ExtractinfosApplication {
                               System.out.println("errTOTAL");
                           }
                       }
-                  //  System.out.println(Tabresult[i]);
                 }
                     System.out.println(result);
 
 
-                } catch (TesseractException e) {
+                } catch (TesseractException | ParseException e) {
                 System.err.println(e.getMessage());
             }
+
+
         System.out.println("Total:   "+Pc.getMontanttotal());
         System.out.println("TOTALHT:   "+Pc.getMontanttotalHT());
         System.out.println("TVA:   "+Pc.getMontantTva());
@@ -192,47 +194,13 @@ public class ExtractinfosApplication {
         System.out.println("DateFacture:   "+Pc.getDatepiececomptable());
 
 
-
-
-
         SpringApplication.run(ExtractinfosApplication.class, args);
     }
-/*  public static void main(String[] args) {
-
-
-      // For complete examples and data files, please go to https://github.com/aspose-ocr/Aspose.OCR-for-Java
-// The path to the documents directory.
-
-// The image path
-      String imagePath = "/home/wajdi/Desktop/PFE/555.png";
-
-//Create api instance
-      AsposeOCR api = new AsposeOCR();
-
-// Recognize page by full path to file
-      try {
-          String result = api.RecognizePage(imagePath);
-          System.out.println("Result: " + result);
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-      SpringApplication.run(ExtractinfosApplication.class, args);
-  }*/
-
-/*
-    public static void main(String[] args) {
-
-
-        try {
-            Platform.setLibPath("/home/wajdi/Downloads/LEADTOOLS21/Bin/Lib/x64");
-            OcrEngine ocrEngine = OcrEngineManager.createEngine(OcrEngineType.LEAD);
-        ocrEngine.startup(null, null, null, null);
-        ocrEngine.getAutoRecognizeManager().run("/home/wajdi/Desktop/PFE/facttest.jpeg", "/home/wajdi/Desktop/PFE/facttest2.pdf",
-                DocumentFormat.PDF, null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        SpringApplication.run(ExtractinfosApplication.class, args);
-
-    }*/
+    public static int getmonthnumber(String month) throws ParseException {
+        Date date = new SimpleDateFormat("MMMM", Locale.FRENCH).parse(month);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        System.out.println(cal.get(Calendar.MONTH));
+        return cal.get(Calendar.MONTH)+1;
+    }
 }
